233b4f5e652fdc15614d5600b094f08dbb671bc4
e-cagan <144911316+e-cagan@users.noreply.github.com>
2025-04-28 18:22:52 +0300

    Update script.py

--- script.py
+++ script.py
@@ -4,9 +4,6 @@ import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
-
 # Initialize the camera
 cap = cv2.VideoCapture(0)
 
